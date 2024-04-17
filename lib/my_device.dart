b93d30e85d1f09c85f524a56e6a22f70434e3177
Thomas-Chambers <70411859+Thomas-Chambers@users.noreply.github.com>
2024-04-16 21:59:32 -0500

    please

--- lib/my_device.dart
+++ lib/my_device.dart
@@ -86,9 +86,12 @@ class MyDevice {
   //data format
   //x y z are 2 bytes each, time is 4 bytes
 
+  static const int max = 2^15-1;
+  static const int sub = 2^16;
+
   int convert(int data){
-    if(data & 0x8000 != 0) {
-      return -((~data & 0xFFFF) + 1);
+    if(data > max){
+      return data - sub;
     }
     return data;
   }
@@ -108,16 +111,6 @@ class MyDevice {
         final correctEvent = [event[1], event[0], event[3], event[2], event[5], event[4], event[7], event[6], event[9], event[8], event[11], event[10], event[15], event[14], event[13], event[12]];
 
         print(correctEvent);
-
-        event[1].toRadixString(16);
-
-        //pad the string with 0s
-
-        print(correctEvent[4] | (correctEvent[5] << 8));
-        print(correctEvent[4] + (correctEvent[5] << 8));
-        print(correctEvent[6] + (correctEvent[7] << 8));
-
-
 
         int gyroX = convert(correctEvent[0] | (correctEvent[1] << 8));
 
