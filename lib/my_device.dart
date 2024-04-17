485f3b61bda99b2683a6f573b850ab98a13a8434
Thomas-Chambers <70411859+Thomas-Chambers@users.noreply.github.com>
2024-04-16 19:17:45 -0500

    fix uuids

--- lib/my_device.dart
+++ lib/my_device.dart
@@ -8,11 +8,11 @@ class MyDevice {
   StreamSubscription<List<int>>? _readSubscription;
   ValueNotifier<bool> isReadyNotifier = ValueNotifier<bool>(false);
 
-  static const String sensorServiceUUID = '0x7147ac18-c824-438e-8506-60829fbd96a3';
+  static const String sensorServiceUUID = '7147ac18-c824-438e-8506-60829fbd96a3';
 
-  static const String sensorDataUUID = '0xbd148149-4469-479a-856f-497ea5e785e5';
+  static const String sensorDataUUID = 'bd148149-4469-479a-856f-497ea5e785e5';
 
-  static const String sensorConfigUUID = '0x95f61667-ffd9-7d9e-fe41-9aed7794ef2f';
+  static const String sensorConfigUUID = '95f61667-ffd9-7d9e-fe41-9aed7794ef2f';
 
   //6 bytes gyro, 6 bytes accel, 4 bytes timestamp 32 bit int
 
